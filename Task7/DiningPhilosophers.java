273af0e9ebf075966795194a3acf14240da2247a
Jack Boyer <jnboyer1@gmail.com>
2021-04-23 17:16:52 +0100

    Changed the number of philosophers back to 5

--- Task7/DiningPhilosophers.java
+++ Task7/DiningPhilosophers.java
@@ -2,7 +2,7 @@ public class DiningPhilosophers {
 
     public static void main(String[] args) throws Exception {
 
-        Philosopher[] philosophers = new Philosopher[3];
+        Philosopher[] philosophers = new Philosopher[5];
         Object[] forks = new Object[philosophers.length];
 
         for (int i = 0; i < forks.length; i++) {
